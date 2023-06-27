c11a4fa678688746596140335a5805f70cd0f7b7
m124578n <m23568n@gmail.com>
2023-06-27 18:01:13 +0800

    add

---
+++ backend/app/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = self.Meta.model(**validated_data)
+        user.set_password(password)
+        user.save(force_insert=True)
+        return user
