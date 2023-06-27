2afdd85f49ef0a88015e718f97c2d6b4243c1d64
m124578n <m23568n@gmail.com>
2023-06-27 10:41:01 +0800

    create simple jwt and swagger

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('', include('app.urls')),
     path('admin/', admin.site.urls),
+    path('api/token/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),     
+    path('api/token/refresh/', views.TokenRefreshView.as_view(), name='token_refresh'), 
+    path('api/token/verify/', views.TokenVerifyView.as_view(), name='token_verify'),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "docs/",
+        SpectacularSwaggerView.as_view(
+            template_name="swagger-ui.html", url_name="schema"
+        ),
+        name="swagger-ui",
+    ),
 ]
