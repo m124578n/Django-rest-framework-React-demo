8c77c2337b3097372049c8d377398d22e2c01fe1
m124578n <m23568n@gmail.com>
2023-06-28 14:21:36 +0800

    add login serializer

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -15,3 +21,20 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save(force_insert=True)
         return user
+    
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+        if username and password:
+            user = authenticate(username=username, password=password)
+        else:
+            raise serializers.ValidationError('require username and password')
+        if not user:
+            raise serializers.ValidationError('unable to login')
+        if not user.is_active:
+            raise serializers.ValidationError('user is disabled')
+        return data
